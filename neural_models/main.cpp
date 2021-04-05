65ecfeb7d976dcfde38e19e1a3881f1ca1d67ecf
Marta <marta.nunes.abreu@gmail.com>
2021-04-05 15:31:28 +0100

    added test code

--- neural_models/main.cpp
+++ neural_models/main.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 #include "perceptron.h"
 
-int main() {
-    srand(time(NULL));
-    rand();
-
+void single_perceptron(void)
+{
     std::cout << "\n\n EXAMPLE\n\n";
 
     Perceptron *p = new Perceptron(2);
@@ -17,6 +15,10 @@ int main() {
     std::cout << p->run({1,0}) << std::endl;
     std::cout << p->run({1,1}) << std::endl;
 
+}
+
+void multi_layer_perceptron(void)
+{  
     std::cout << "\n\n --- Hardcoded XOR example --- \n\n";
     MultiLayerPerceptron mlp = MultiLayerPerceptron({2,2,1});
     mlp.set_weights({{{-10,-10,15},{15,15,-10}},{{10,10,-15}}});
@@ -29,5 +31,46 @@ int main() {
     std::cout << "1 0 = " << mlp.run({1,0})[0] << std::endl;
     std::cout << "1 1 = " << mlp.run({1,1})[0] << std::endl;
 
+}
+
+void multi_layer_training(void)
+{
+    std::cout << "\n\n --- Trained XOR Example --- \n\n";
+    MultiLayerPerceptron mlp = MultiLayerPerceptron({2,2,1});
+
+    double MSE;
+    for (int i = 0; i < 3000; i++){
+        MSE = 0.0;
+        MSE += mlp.backpropagation({0,0},{0});
+        MSE += mlp.backpropagation({0,1},{1});
+        MSE += mlp.backpropagation({1,0},{1});
+        MSE += mlp.backpropagation({1,1},{0});
+        MSE = MSE / 4.0;
+        if ( i % 100 == 0)
+            std::cout << "MSE = " << MSE << std::endl;
+    }
+
+    std::cout << "\n Trained weights: \n \n";
+    mlp.print_weights();
+
+    std::cout << "XOR: " <<  std::endl;
+    std::cout << "0 0 = " << mlp.run({0,0})[0] << std::endl;
+    std::cout << "0 1 = " << mlp.run({0,1})[0] << std::endl;
+    std::cout << "1 0 = " << mlp.run({1,0})[0] << std::endl;
+    std::cout << "1 1 = " << mlp.run({1,1})[0] << std::endl;
+
+
+
+}
+
+int main() {
+    srand(time(NULL));
+    rand();
+
+    single_perceptron();
+    multi_layer_perceptron();
+
+    multi_layer_training();
+ 
 
 }
