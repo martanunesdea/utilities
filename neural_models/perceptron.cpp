0e29846dc999b1baa0fe74c66ff291c464dd54d5
Marta <marta.nunes.abreu@gmail.com>
2021-04-05 12:50:58 +0100

    multilayer perceptron class

--- neural_models/perceptron.cpp
+++ neural_models/perceptron.cpp
@@ -5,16 +5,6 @@ double frand(){
     return ( 2.0*(double)rand() / RAND_MAX - 1.0);
 }
 
-void Perceptron::set_weights(std::vector<double> w_init)
-{
-    this->weights = w_init;
-}
-
-double Perceptron::sigmoid(double x)
-{
-    return ( 1.0 / (1.0 + exp(-x)));
-}
-
 Perceptron::Perceptron(int inputs, double bias){
     this->bias = bias;
     weights.resize(inputs+1);
@@ -25,5 +15,30 @@ double Perceptron::run(std::vector<double> x){
     x.push_back(bias);
     double sum = inner_product(x.begin(), x.end(), weights.begin(), (double)0.0);
     return sigmoid(sum);
+}
+
+void Perceptron::set_weights(std::vector<double> w_init)
+{
+    this->weights = w_init;
+}
+
+double Perceptron::sigmoid(double x)
+{
+    return ( 1.0 / (1.0 + exp(-x)));
+}
+
+MultiLayerPerceptron::MultiLayerPerceptron(std::vector<int> layers, double bias, double eta) {
+    this->layers = layers;
+    this->bias = bias;
+    this->eta = eta;
+
+    for ( int i = 0; i < layers.size(); i++ )
+    {
+        values.push_back(std::vector<double>(layers[i],0.0));
+        network.push_back(std::vector<Perceptron>());
+        if ( i > 0 ) 
+            for (int j = 0; j < layers[i]; j++)
+                network[i].push_back(Perceptron(layers[i-1],bias));
+    }
 }
 
--- neural_models/perceptron.h
+++ neural_models/perceptron.h
@@ -15,3 +15,20 @@ class Perceptron {
     double sigmoid(double x);
     double run(std::vector<double> x);
 };
+
+
+class MultiLayerPerceptron {
+    public:
+        MultiLayerPerceptron(std::vector<int> layers, double bias=1.0, double eta=0.5);
+        void set_weights(std::vector<std::vector<std::vector<double> > > w_init);
+        void print_weights();
+        std::vector<double> run(std::vector<double> x);
+        double bp(std::vector<double> x, std::vector<double> y);
+
+        std::vector<int> layers;
+        double bias;
+        double eta;
+        std::vector<std::vector<Perceptron> > network;
+        std::vector<std::vector<double> > values;
+        std::vector<std::vector<double> > d;
+}
